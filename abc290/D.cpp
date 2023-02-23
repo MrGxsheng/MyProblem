ff950598a55d50e0610c436b80cb99f6a31f205d
xsheng <2838428014@qq.com>
2023-02-23 10:46:44 +0800

    abc290

---
+++ abc290/D.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define debug puts("-----")
+#define eps (1e-8)
+#define inf (1<<30)
+#define fi first
+#define se second
+#define endl '\n'
+using i64 = long long;
+ 
+void solve() {
+    int n, d, k;
+    std::cin >> n >> d >> k;
+    
+    k--;
+    
+    int g = n / std::gcd(n, d);
+    std::cout << (1LL * d * (k % g) + k / g) % n << "\n";
+}
+ 
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int t;
+    std::cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+/*
+*
+*　　┏┓　　　┏┓+ +
+*　┏┛┻━━━┛┻┓ + +
+*　┃　　　　　　　┃
+*　┃　　　━　　　┃ ++ + + +
+*  ████━████+
+*  ◥██◤　◥██◤ +
+*　┃　　　┻　　　┃
+*　┃　　　　　　　┃ + +
+*　┗━┓　　　┏━┛
+*　　　┃　　　┃ + + + +Code is far away from 　
+*　　　┃　　　┃ + bug with the animal protecting
+*　　　┃　 　 ┗━━━┓ 神兽保佑,代码无bug　
+*　　　┃ 　　　　　　 ┣┓
+*　　  ┃ 　　　　　 　┏┛
+*　    ┗┓┓┏━┳┓┏┛ + + + +
+*　　　　┃┫┫　┃┫┫
+*　　　　┗┻┛　┗┻┛+ + + +
+*/
+
+//        _                                    _  _  _         _                   
+//       | |                                  (_)| || |       | |                  
+//   ___ | |__    ___  _ __    __ _ __      __ _ | || |  __ _ | | __    ___  _ __  
+//  / __|| '_ \  / _ \| '_ \  / _` |\ \ /\ / /| || || | / _` || |/ /   / __|| '_ \ 
+//  \__ \| | | ||  __/| | | || (_| | \ V  V / | || || || (_| ||   <  _| (__ | | | |
+//  |___/|_| |_| \___||_| |_| \__, |  \_/\_/  |_||_||_| \__,_||_|\_\(_)\___||_| |_|
+//                             __/ |                                               
+//                            |___/                                             
