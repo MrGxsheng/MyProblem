f30d081a85600c4b5d90e3090af2130a61759511
xsheng <2838428014@qq.com>
2023-03-10 15:25:03 +0800

    cf1800div3

---
+++ cf1800-div3/B.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define debug puts("-----")
+#define eps (1e-8)
+#define inf (1<<30)
+#define fi first
+#define se second
+#define endl '\n'
+using namespace std;
+typedef long long LL;
+typedef pair<int,int> PII;
+const int N = 1000010;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    
+    int lo[26] {}, up[26] {};
+    string s;
+    cin >> s;
+    
+    for (auto c : s) {
+        if (islower(c)) {
+            lo[c - 'a'] += 1;
+        } else {
+            up[c - 'A'] += 1;
+        }
+    }
+    
+    int max = 0;
+    int ans = 0;
+    for (int i = 0; i < 26; i++) {
+        ans += min(lo[i], up[i]);
+        max += (lo[i] + up[i]) / 2;
+    }
+    
+    ans = min(ans + k, max);
+    cout << ans << "\n";
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+	int T;
+	cin >> T;
+	while(T--) solve();
+	return 0;
+}
+
+/*
+*
+*　　┏┓　　　┏┓+ +
+*　┏┛┻━━━┛┻┓ + +
+*　┃　　　　　　　┃
+*　┃　　　━　　　┃ ++ + + +
+*  ████━████+
+*  ◥██◤　◥██◤ +
+*　┃　　　┻　　　┃
+*　┃　　　　　　　┃ + +
+*　┗━┓　　　┏━┛
+*　　　┃　　　┃ + + + +Code is far away from 　
+*　　　┃　　　┃ + bug with the animal protecting
+*　　　┃　 　 ┗━━━┓ 神兽保佑,代码无bug　
+*　　　┃ 　　　　　　 ┣┓
+*　　  ┃ 　　　　　 　┏┛
+*　    ┗┓┓┏━┳┓┏┛ + + + +
+*　　　　┃┫┫　┃┫┫
+*　　　　┗┻┛　┗┻┛+ + + +
+*/
+
+//        _                                    _  _  _         _                   
+//       | |                                  (_)| || |       | |                  
+//   ___ | |__    ___  _ __    __ _ __      __ _ | || |  __ _ | | __    ___  _ __  
+//  / __|| '_ \  / _ \| '_ \  / _` |\ \ /\ / /| || || | / _` || |/ /   / __|| '_ \ 
+//  \__ \| | | ||  __/| | | || (_| | \ V  V / | || || || (_| ||   <  _| (__ | | | |
+//  |___/|_| |_| \___||_| |_| \__, |  \_/\_/  |_||_||_| \__,_||_|\_\(_)\___||_| |_|
+//                             __/ |                                               
+//                            |___/                                             
---
+++ cf1800-div3/E.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define debug puts("-----")
+#define eps (1e-8)
+#define inf (1<<30)
+#define fi first
+#define se second
+#define endl '\n'
+using namespace std;
+typedef long long LL;
+typedef pair<int,int> PII;
+const int N = 1000010;
+int n,m;
+void solve(){
+	cin >> n >> m;
+	string s,t;
+	cin >> s >> t;
+
+	int cnt[26] {};
+	for(int i = 0 ; i < n ; i++){
+		if(i - m < 0 && i + m >= n){
+			if(s[i] != t[i]){
+				cout << "NO" << endl;
+				return;
+			}
+		}else{
+			cnt[s[i] - 'a'] += 1;
+			cnt[t[i] - 'a'] -= 1;
+		}
+	}
+
+	for(int i = 0 ; i < 26 ; i++){
+		if(cnt[i] != 0){
+			cout << "NO" << endl;
+			return;
+		}
+	}
+	cout << "YES" << endl; 
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+	int T;
+	cin >> T;
+	while(T--) solve();
+	return 0;
+}
+
+/*
+*
+*　　┏┓　　　┏┓+ +
+*　┏┛┻━━━┛┻┓ + +
+*　┃　　　　　　　┃
+*　┃　　　━　　　┃ ++ + + +
+*  ████━████+
+*  ◥██◤　◥██◤ +
+*　┃　　　┻　　　┃
+*　┃　　　　　　　┃ + +
+*　┗━┓　　　┏━┛
+*　　　┃　　　┃ + + + +Code is far away from 　
+*　　　┃　　　┃ + bug with the animal protecting
+*　　　┃　 　 ┗━━━┓ 神兽保佑,代码无bug　
+*　　　┃ 　　　　　　 ┣┓
+*　　  ┃ 　　　　　 　┏┛
+*　    ┗┓┓┏━┳┓┏┛ + + + +
+*　　　　┃┫┫　┃┫┫
+*　　　　┗┻┛　┗┻┛+ + + +
+*/
+
+//        _                                    _  _  _         _                   
+//       | |                                  (_)| || |       | |                  
+//   ___ | |__    ___  _ __    __ _ __      __ _ | || |  __ _ | | __    ___  _ __  
+//  / __|| '_ \  / _ \| '_ \  / _` |\ \ /\ / /| || || | / _` || |/ /   / __|| '_ \ 
+//  \__ \| | | ||  __/| | | || (_| | \ V  V / | || || || (_| ||   <  _| (__ | | | |
+//  |___/|_| |_| \___||_| |_| \__, |  \_/\_/  |_||_||_| \__,_||_|\_\(_)\___||_| |_|
+//                             __/ |                                               
+//                            |___/                                             
